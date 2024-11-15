8b958b7d503a8b2d71386ef635128c3a98ae6cf1
Yaniv Bialik <yanivv77@gmail.com>
2024-11-15 18:19:01 +0200

    Update subcategory page

---
+++ src/app/(category-sidebar)/products/[category]/loading.tsx
@@ -0,0 +1,10 @@
+import { LoadingPage } from "@/components/loading";
+
+/**
+ * v0 by Vercel.
+ * @see https://v0.dev/t/Z1bVbMZYEOI
+ * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
+ */
+export default function Page() {
+  return <LoadingPage />;
+}
