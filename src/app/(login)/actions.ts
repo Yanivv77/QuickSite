3e394ece021da9abf9d507598ead35cc2733e456
Yaniv Bialik <yanivv77@gmail.com>
2024-11-15 19:18:41 +0200

    ADD authRateLimit and signUpRateLimit with redis

--- src/app/(login)/actions.ts
+++ src/app/(login)/actions.ts
@@ -2,46 +2,31 @@
 
 import { z } from "zod";
 import { eq } from "drizzle-orm";
-import { cookies } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { validatedAction } from "@/lib/middleware";
 import { db } from "@/db";
 import { NewUser, users } from "@/db/schema";
 import { comparePasswords, hashPassword, setSession } from "@/lib/session";
+import { authRateLimit ,signUpRateLimit} from "@/lib/rate-limit";
+
 
 const authSchema = z.object({
   username: z.string().min(1),
   password: z.string().min(1),
-  mode: z.enum(["signin", "signup"]),
 });
 
-async function signIn(input: z.infer<typeof authSchema>) {
-  const { username, password } = input
-  const user = await db
-    .select({
-      user: users,
-    })
-    .from(users)
-    .where(eq(users.username, username))
-    .limit(1);
-
-  if (user.length === 0) {
-    return { error: "Invalid email or password. Please try again." };
+export const signUp = validatedAction(authSchema, async (data) => {
+  const { username, password } = data;
+  const ip = (await headers()).get("x-real-ip") ?? "local";
+  const rl2 = await signUpRateLimit.limit(ip);
+  if (!rl2.success) {
+    return {
+      error: {
+        code: "AUTH_ERROR",
+        message: "Too many signups. Try again later",
+      },
+    };
   }
-
-  const { user: foundUser } = user[0];
-
-  const isPasswordValid = await comparePasswords(
-    password,
-    foundUser.passwordHash,
-  );
-
-  if (!isPasswordValid) {
-    return { error: "Invalid email or password. Please try again." };
-  }
-  await setSession(foundUser);
-}
-async function signUp(input: z.infer<typeof authSchema>) {
-  const { username, password } = input;
 
   const existingUser = await db
     .select()
@@ -50,7 +35,7 @@ async function signUp(input: z.infer<typeof authSchema>) {
     .limit(1);
 
   if (existingUser.length > 0) {
-    return { error: "Failed to create user. Please try again." };
+    return { error: "Username already taken. Please try again." };
   }
 
   const passwordHash = await hashPassword(password);
@@ -66,17 +51,48 @@ async function signUp(input: z.infer<typeof authSchema>) {
     return { error: "Failed to create user. Please try again." };
   }
   await setSession(createdUser);
-}
-export const signInSignUp = validatedAction(authSchema, async (data) => {
-  const { mode } = data;
-  if(mode === "signin") {
-    return signIn(data);
-  } else {
-    return signUp(data);
-  }
 });
 
+export const signIn = validatedAction(authSchema, async (data) => {
+  const { username, password } = data;
+  const ip = (await headers()).get("x-real-ip") ?? "local";
+  const rl = await authRateLimit.limit(ip);
+
+  if (!rl.success) {
+    return {
+      error: {
+        code: "AUTH_ERROR",
+        message: "Too many attempts. Try again later",
+      },
+    };
+  }
+  const user = await db
+    .select({
+      user: users,
+    })
+    .from(users)
+    .where(eq(users.username, username))
+    .limit(1);
+
+  if (user.length === 0) {
+    return { error: "Invalid username or password. Please try again." };
+  }
+
+  const { user: foundUser } = user[0];
+
+  const isPasswordValid = await comparePasswords(
+    password,
+    foundUser.passwordHash,
+  );
+
+  if (!isPasswordValid) {
+    return { error: "Invalid username or password. Please try again." };
+  }
+  await setSession(foundUser);
+});
 
 export async function signOut() {
-  (await cookies()).delete("session");
+  // clear session & cart
+  const c = await cookies();
+  c.getAll().forEach((cookie) => c.delete(cookie.name));
 }
