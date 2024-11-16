cee450024d4f1632304c0e53874a114f9ed0ea94
Yaniv Bialik <yanivv77@gmail.com>
2024-11-16 18:00:56 +0200

    update order history

--- src/app/order-history/page.tsx
+++ src/app/order-history/page.tsx
@@ -1,19 +1,22 @@
-import { LoginForm } from "@/components/login-form";
 import { Metadata } from "next";
+import { Suspense } from "react";
+import { OrderHistoryDynamic } from "./dynamic";
+
 
 export const metadata: Metadata = {
   title: "Order History",
 };
 
-export default function Page() {
+export default async function Page() {
   return (
     <main className="min-h-screen p-4">
-      <div className="mx-auto flex max-w-md flex-col gap-4">
-        <h1 className="font-futura text-2xl text-green-800">ORDER HISTORY</h1>
-        <p className="font-semibold text-green-800">
-          Log in to view order history
-        </p>
-        <LoginForm />
+      <h1 className="w-full border-b-2 border-accent1 text-left text-2xl text-accent1">
+        Order History
+      </h1>
+      <div className="mx-auto flex max-w-md flex-col gap-4 text-black">
+        <Suspense>
+          <OrderHistoryDynamic />
+        </Suspense>
       </div>
     </main>
   );
